b718678fd2b070e330e0965b89f85dc5f78e2dfc
jayantibhandari <ursenseisenyor17@gmail.com>
2023-08-30 13:04:20 +0545

    completed marurice

---
+++ faqs.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Font Awesome -->
+<link
+  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+  rel="stylesheet"
+/>
+<!-- Google Fonts -->
+<link
+  href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+  rel="stylesheet"
+/>
+<!-- MDB -->
+<link
+  href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.1/mdb.min.css"
+  rel="stylesheet"
+/>
+<!-- MDB -->
+<script
+  type="text/javascript"
+  src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.1/mdb.min.js"
+></script>
+    <title>Document</title>
+
+    <style>
+        .faqs{
+            margin: 5% 20%;
+            font-size: 20px;
+            font-weight: 600;
+            color: black;
+        }
+        .faqs h2{
+            font-size: 25px;
+            font-weight: 600;
+        }
+    </style>
+</head>
+<body>
+<?php include 'header.php '  ?>
+<div>
+    <div class="faqs">
+    <div class="accordion w-100" id="basicAccordion">
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingOne">
+      <button class="accordion-button collapsed" type="button" data-mdb-toggle="collapse"
+        data-mdb-target="#basicAccordionCollapseOne" aria-expanded="false" aria-controls="collapseOne">
+        1. What is marriage registration, and why is it important?
+      </button>
+    </h2>
+    <div id="basicAccordionCollapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne"
+      data-mdb-parent="#basicAccordion" style="">
+      <div class="accordion-body">
+      Marriage registration is the process of legally documenting a marriage with the government. It serves as proof of the marital relationship and offers legal rights and benefits to the married couple. It also helps maintain records for various legal and administrative purposes.
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingTwo">
+      <button class="accordion-button collapsed" type="button" data-mdb-toggle="collapse"
+        data-mdb-target="#basicAccordionCollapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+        2. Is marriage registration mandatory in Nepal?
+      </button>
+    </h2>
+    <div id="basicAccordionCollapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo"
+      data-mdb-parent="#basicAccordion" style="">
+      <div class="accordion-body">
+      Yes, marriage registration is mandatory in Nepal. The Marriage Registration Act of Nepal requires all marriages to be registered with the appropriate authority.
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingTwo">
+      <button class="accordion-button collapsed" type="button" data-mdb-toggle="collapse"
+        data-mdb-target="#basicAccordionCollapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+        3. Who is responsible for overseeing marriage registration in Nepal?
+      </button>
+    </h2>
+    <div id="basicAccordionCollapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo"
+      data-mdb-parent="#basicAccordion" style="">
+      <div class="accordion-body">
+      Marriage registration in Nepal is overseen by the local government bodies, usually the Village Development Committee (VDC), Municipality, or Rural Municipality, depending on the location of the marriage.  </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingTwo">
+      <button class="accordion-button collapsed" type="button" data-mdb-toggle="collapse"
+        data-mdb-target="#basicAccordionCollapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+        4. What is the procedure for marriage registration in Nepal?
+      </button>
+    </h2>
+    <div id="basicAccordionCollapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo"
+      data-mdb-parent="#basicAccordion" style="">
+      <div class="accordion-body">
+      The procedure generally involves submitting an application form along with required documents to the local authority. Both parties, along with witnesses, need to be present during the registration. After verification, a marriage certificate is issued.   </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingTwo">
+      <button class="accordion-button collapsed" type="button" data-mdb-toggle="collapse"
+        data-mdb-target="#basicAccordionCollapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+        5.  What are the documents required for marriage registration?
+      </button>
+    </h2>
+    <div id="basicAccordionCollapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo"
+      data-mdb-parent="#basicAccordion" style="">
+      <div class="accordion-body">
+      The common documents required include citizenship certificates of both parties, passport-sized photographs, and a joint application form. In the case of individuals from different countries, additional documents may be needed, such as a "No Objection Certificate" from the respective embassies.   </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingTwo">
+      <button class="accordion-button collapsed" type="button" data-mdb-toggle="collapse"
+        data-mdb-target="#basicAccordionCollapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+        6. Can foreigners get married in Nepal?
+      </button>
+    </h2>
+    <div id="basicAccordionCollapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo"
+      data-mdb-parent="#basicAccordion" style="">
+      <div class="accordion-body">
+      Yes, foreigners can get married in Nepal, but they need to follow specific procedures. They may need to provide additional documents, such as a single status certificate, passport copies, and other relevant documents.  </div>
+    </div>
+  </div>
+
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingTwo">
+      <button class="accordion-button collapsed" type="button" data-mdb-toggle="collapse"
+        data-mdb-target="#basicAccordionCollapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+        7. What is a joint application form, and where can I get it?
+      </button>
+    </h2>
+    <div id="basicAccordionCollapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo"
+      data-mdb-parent="#basicAccordion" style="">
+      <div class="accordion-body">
+      A joint application form is a document that both parties intending to get married need to fill out. It can usually be obtained from the local government office or municipality where you intend to register the marriage.   </div>
+    </div>
+  </div>
+  
+  </div>
+</div>
+    </div>
+</div>
+<?php include 'footer.php '  ?>
+</body>
+</html>
--- header.php
+++ header.php
@@ -34,7 +34,7 @@
 
       <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
         <li><a href="index.php" class="nav-link px-2 text-white">Home</a></li>
-        <li><a href="#" class="nav-link px-2 text-white">FAQs</a></li>
+        <li><a href="faqs.php" class="nav-link px-2 text-white">FAQs</a></li>
         <li><a href="contact.php" class="nav-link px-2 text-white">Contact Us</a></li>
       </ul>
 <!--
